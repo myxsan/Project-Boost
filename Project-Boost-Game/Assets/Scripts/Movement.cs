404e8f62401a5f9ef3aaf98d3a209d013dd56c58
myxsan <yksl3461@gmail.com>
2022-04-19 23:48:30 +0300

    Created prefabs and movement script (impemented it) for the rocket

---
+++ Project-Boost-Game/Assets/Scripts/Movement.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float thrustPower;
+    [SerializeField] float rotateAmount;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessThrust()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Thrust");
+            rb.AddRelativeForce(Vector3.up * thrustPower * Time.deltaTime);
+        }
+    }
+    private void ProcessRotation()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotateAmount);
+
+            Debug.Log("Left Rotated");
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotateAmount);
+            Debug.Log("Right Rotated");
+        }
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+    }
+}
